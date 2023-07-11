6a4a89397dbd7e99e51f8f7142d22da237585e5b
Deeptanu-Dhar <deepdhar892@gmail.com>
2023-07-12 01:26:22 +0530

    Contact us page added

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// scroll to top action 
+// -----------------scroll to top action------------------- 
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
@@ -8,3 +8,33 @@ window.addEventListener("scroll", () => {
 		toTop.classList.remove("active");
 	}
 })
+
+
+// -------------------------Shadow for newsletter form---------------------------
+
+const newsForm = document.getElementById("news-form");
+
+newsForm.addEventListener("click",()=>{
+	newsForm.classList.add("shadow-style");
+});
+
+document.addEventListener("click", (event) => {
+  if (!newsForm.contains(event.target)){
+    newsForm.classList.remove("shadow-style");
+  }
+});
+
+// -------------------------Shadow for newsletter form---------------------------
+
+
+const contactForm = document.getElementById("form-container");
+
+contactForm.addEventListener("click",()=>{
+	contactForm.classList.add("shadow-style2");
+});
+
+document.addEventListener("click", (event) => {
+  if (!contactForm.contains(event.target)){
+    contactForm.classList.remove("shadow-style2");
+  }
+});
